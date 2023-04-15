f781d653b2b0654e91d69dc57e4c03600b0e92eb
MatthewHier <mhier@cronsrud.com>
2023-04-15 19:21:40 -0400

    Added Program List / Activation / Start

--- ReadFanucProgramDemo/Program.cs
+++ ReadFanucProgramDemo/Program.cs
@@ -11,69 +11,55 @@ namespace ReadFanucProgramDemo
 {
     internal class Program
     {
-        static ushort _handle = 0;
-        static short _ret = 0;
+        private static short _ret = 0;
+        private static Fanuc _fanuc;
+
 
         static void Main(string[] args)
         {
 
-            _ret = Focas1.cnc_allclibhndl3("192.168.2.123", 8193, 6, out _handle);
+            _fanuc = new Fanuc();
+            var handle = _fanuc.Connect("192.168.2.123");
 
-            if (_ret != Focas1.EW_OK)
+            var progList = _fanuc.Programs.GetProgramListing(handle);
+
+            int index = 0;
+
+            foreach (var prg in progList)
             {
-                Console.WriteLine($"Unable to connect to 192.168.2.123 on port 8193\n\nReturn Code: {_ret}\n\nExiting....");
-                Console.Read();
+                Console.WriteLine($"{index}) Name: {prg.Key}\tComment: {prg.Value}");
+                index++;
+            }
+
+            Console.WriteLine("\nPlease Select a program number to activate");
+            var num = Console.ReadLine();
+
+            var activateSuccess = _fanuc.Programs.ActiveMainProgram(handle, progList.ElementAt(int.Parse(num)).Key);
+
+            if (activateSuccess)
+            {
+                Console.WriteLine($"{progList.ElementAt(int.Parse(num)).Key} successfully activated. Do you wish to start the program (Y/N)? ");
+                var startPrg = Console.ReadLine();
+
+                if (startPrg == "Y")
+                {
+                    var startSuccess = _fanuc.Programs.StartProgram(handle);
+                    if (startSuccess)
+                        Console.WriteLine($"{progList.ElementAt(int.Parse(num)).Key} successfully started!");
+                    else
+                        Console.WriteLine($"Unable to start {progList.ElementAt(int.Parse(num)).Key}");
+                }
+                else
+                {
+                    Console.WriteLine("You can start the program by pressing the 'Cycle Start' button on the operator panel");
+                }
             }
             else
             {
-                Console.WriteLine($"Our Focas handle is {_handle}\n\n");
-
-
-                while (true)
-                {
-
-                    Console.Write($"Program Name: {GetProgramName()}\tSub-Program Name: {GetSubProgramName()}                             \r");
-
-                    Thread.Sleep(500);
-                }
-
+                Console.WriteLine($"Unable to activate '{progList.ElementAt(int.Parse(num)).Key}'");
             }
 
+            Console.ReadLine();
         }
-
-        public static string GetProgramName()
-        {
-            if (_handle == 0)
-                return "UNAVAILABLE";
-
-            Focas1.ODBEXEPRG rdProg = new Focas1.ODBEXEPRG();
-
-            _ret = Focas1.cnc_exeprgname(_handle, rdProg);
-
-            if (_ret != Focas1.EW_OK)
-                return _ret.ToString();
-
-            return new string(rdProg.name).Trim('\0');
-        }
-
-
-        public static string GetSubProgramName()
-        {
-            if (_handle == 0)
-                return "UNAVAILABLE";
-
-            Focas1.ODBPRO subProg = new Focas1.ODBPRO();
-
-            _ret = Focas1.cnc_rdprgnum(_handle, subProg);
-
-            if (_ret != Focas1.EW_OK)
-                return _ret.ToString();
-
-            if (subProg.data != subProg.mdata)
-                return subProg.data.ToString();
-            return "No Sub Program";
-
-        }
-
     }
 }
